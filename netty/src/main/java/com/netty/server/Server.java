cc98c60d0d9c4d6c6f7ec5a6bcbc6caabab3dca0
sun <837544713@qq.com>
2021-04-24 10:17:28 +0800

    add netty java

---
+++ netty/src/main/java/com/netty/server/Server.java
@@ -0,0 +1,27 @@
+package com.netty.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.util.concurrent.EventExecutor;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.ScheduledFuture;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+public class Server {
+    public static void main(String[] args) {
+        ServerBootstrap bootstrap = new ServerBootstrap();
+        EventLoopGroup loopGroup = new NioEventLoopGroup();
+        bootstrap.group(loopGroup)
+                 .channel(NioServerSocketChannel.class);
+
+    }
+}
